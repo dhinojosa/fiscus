1ddfdaed35cc9a571d2e375fd04a098c09147d7e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-06-28 13:40:03 -0600

    added src

---
+++ src/main/scala/com/evolutionnext/fiscus/Fiscus.scala
@@ -0,0 +1,54 @@
+package com.evolutionnext.fiscus
+
+import java.lang.RuntimeException
+
+/**
+ * Created by Daniel Hinojosa
+ * User: Daniel Hinojosa
+ * Date: 6/23/11
+ * Time: 2:42 PM
+ * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
+ * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
+ * tel: 505.363.5832
+ */
+
+trait Fiscus {
+  val contents: Seq[Any] = Nil
+
+  def run() {
+    //    contents.flatMap(x => x)
+    //    labelMap = contents.flatMap {
+    //      _ match {
+    //        case x: Wrapper[_] => Some((x.label, x))
+    //        case _ => None
+    //      }
+    //    }
+  }
+
+  def get(label: String) = {
+    println(contents)
+    contents.filter(_.isInstanceOf[Tuple2[_,String]]).asInstanceOf[Seq[Tuple2[_,String]]].find(_._2 == label) match {
+      case Some((value, str)) => value
+      case None => throw new RuntimeException("Not Found")
+    }
+  }
+
+  //
+  //  def get[T](clazz: Class[T])(implicit manifest: Manifest[T]) = {
+  //    manifest.erasure.getConstructors.find(_.getParameterTypes.size == 0) match {
+  //      case Some(c) => c.newInstance()
+  //      case None => throw new RuntimeException("No Way To Construct Object")
+  //    }
+  //  }
+}
+
+object Fiscus {
+
+    class Wrapper(val item:Any) {
+      def labeledAs(label: String) = (item, label)
+    }
+
+    implicit def convertAnyToWrapper(t: Any): Wrapper = {
+      new Wrapper(t)
+    }
+}
---
+++ src/test/scala/com/evolutionnext/fiscus/FiscusTest.scala
@@ -0,0 +1,55 @@
+package com.evolutionnext.fiscus
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+
+
+/**
+ * Created by Daniel Hinojosa
+ * User: Daniel Hinojosa
+ * Date: 6/23/11
+ * Time: 1:17 PM
+ * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
+ * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
+ * tel: 505.363.5832
+ */
+class FiscusTest extends WordSpec with ShouldMatchers {
+
+  "A Fiscus" should {
+    "spit back out an instance of an object given a set of classes with one as a label" in {
+
+      import com.evolutionnext.fiscus.Fiscus._
+
+      class Brown
+      class Green
+      class Blue
+
+      object ColorTest extends Fiscus {
+        override val contents = (new Brown labeledAs "favorite color") :: new Green :: new Blue :: Nil
+
+        override def run() {
+          get("favorite color").asInstanceOf[Brown].getClass should be (classOf[Brown])
+        }
+      }
+
+      ColorTest.run()
+    }
+  }
+}
+
+//
+//    "return an object if that object has a no args constructor" in {
+//
+//      class Brown
+//      class Green
+//      class Blue
+//
+//      object ColorTest extends Fiscus {
+//        override val contents = new Brown :: new Green :: new Blue :: Nil
+//
+//        override def run() {
+//          getC(classOf[Brown])
+//        }
+//      }
+//      ColorTest.run()
+//    }
